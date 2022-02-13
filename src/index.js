c0253d3f9810d9f69cde600714359fcea4f7957a
salangdung-i <dladmswl92@naver.com>
2022-02-13 15:57:18 +0900

    18.2 미들웨어

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger(); // 오픈 소스 커뮤니티에 있는 redux-logger 미들웨어 사용
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// 만든 리덕스 미들웨어를 스토어에 적용하기
