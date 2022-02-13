f13d763103aa17e34a02b4c2c3180ac249791a7b
salangdung-i <dladmswl92@naver.com>
2022-02-13 17:18:53 +0900

    18.3 비동기 작업을 처리하는 미들웨어 사용

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
-import loggerMiddleware from './lib/loggerMiddleware';
+// import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger(); // 오픈 소스 커뮤니티에 있는 redux-logger 미들웨어 사용
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -6,6 +6,19 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함 
+export const increaseAsync = () => dispatch => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => dispatch => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
+
 const initialState = 0; // 상태는 꼭 객체일 필요없다. 숫자도 괜춘
 
 const counter = handleActions(
@@ -17,3 +30,7 @@ const counter = handleActions(
 );
 
 export default counter;
+
+// redux-thunk는 액션 생성함수에서 일반 액션 객체를 반화하는 대신에 함수를 반환한다. 
+// increaseAsync, decreaseAsync 함수를 만들어 카운터 값을 비동기적으로 한번더 변경
+
