7fbc4ac4f69771250f9b60b812b000539b13511d
salangdung-i <dladmswl92@naver.com>
2022-04-13 10:29:43 +0900

    프로젝트 초기화

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-// import loggerMiddleware from './lib/loggerMiddleware';
-import { createLogger } from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
 
-const logger = createLogger(); // 오픈 소스 커뮤니티에 있는 redux-logger 미들웨어 사용
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,5 +14,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// 만든 리덕스 미들웨어를 스토어에 적용하기
