c845c612a549fba08f2e43fdc6352ecb684abdfa
salangdung-i <dladmswl92@naver.com>
2022-04-13 11:54:38 +0900

    비동기 작업을 처리하는 미들웨어 사용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { increase, decrease } from '../modules/counter';
+import { increaseAsync, decreaseAsync } from '../modules/counter';
 import Counter from '../components/Counter';
 import { connect } from 'react-redux';
 
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={increaseAsync} onDecrease={decreaseAsync} />
   );
 };
 
@@ -15,7 +15,7 @@ export default connect(
     number: state.counter
   }),
   {
-    increase,
-    decrease
+    increaseAsync,
+    decreaseAsync
   }
 )(CounterContainer);
